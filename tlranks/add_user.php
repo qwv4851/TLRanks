502f37abd62b72c2863ddd4fc8f93353c4126f86
Sean Reilly <qwv4851@gmail.com>
2013-03-17 07:39:13 -0700

    Fixed issue where players with no modes would cause an error in add_user.php.

--- tlranks/add_user.php
+++ tlranks/add_user.php
@@ -8,10 +8,12 @@ try {
   $stmt = $db->prepare("REPLACE INTO users (name, region, profile, date) VALUES (?, ?, ?, CURDATE())");
   $stmt->execute($data);
   
-  foreach($_POST['modes'] as &$mode) {
-    $data = array($_POST['name'], $mode['name'], $mode['leagueIndex'], $mode['tier'], $mode['points'], $mode['race'], $mode['wins'], $mode['losses'], $mode['divisionRank'], $mode['regionRank'], $mode['worldRank'], $mode['game']);
-    $stmt = $db->prepare("REPLACE INTO mode (name, mode, leagueIndex, tier, points, race, wins, losses, divisionRank, regionRank, worldRank, game, date) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, CURDATE())");
-    $stmt->execute($data);
+  if (isset($_POST['modes'])) {
+    foreach($_POST['modes'] as $mode) {
+      $data = array($_POST['name'], $mode['name'], $mode['leagueIndex'], $mode['tier'], $mode['points'], $mode['race'], $mode['wins'], $mode['losses'], $mode['divisionRank'], $mode['regionRank'], $mode['worldRank'], $mode['game']);
+      $stmt = $db->prepare("REPLACE INTO mode (name, mode, leagueIndex, tier, points, race, wins, losses, divisionRank, regionRank, worldRank, game, date) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, CURDATE())");
+      $stmt->execute($data);
+    }
   }
   
 } catch(PDOException $e) {
