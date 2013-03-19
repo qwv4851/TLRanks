af8d4649ae5afc18707bd0ecdd270ffab882b6d0
Sean Reilly <qwv4851@gmail.com>
2013-03-18 22:58:24 -0700

    Added graphs.

---
+++ tlranks/get_data.php
@@ -0,0 +1,55 @@
+<?php
+header("content-type: application/json"); 
+require 'database.php';
+  try {
+    $db = dbConnect();        
+    
+    $stmt = $db->prepare("SELECT COUNT(*) FROM mode GROUP BY mode");
+    $stmt->setFetchMode(PDO::FETCH_NUM);
+    $stmt->execute();
+    $jsonData['modes'] = $stmt->fetchAll(PDO::FETCH_COLUMN, 0);
+    
+    $stmt = $db->prepare("SELECT COUNT(*) FROM mode WHERE mode=1 GROUP BY leagueIndex");
+    $stmt->setFetchMode(PDO::FETCH_NUM);
+    $stmt->execute();
+    $jsonData['leagues'] = $stmt->fetchAll(PDO::FETCH_COLUMN, 0);
+    
+    $jsonData['teams'] = array();
+    
+    $stmt = $db->prepare("SELECT COUNT(*) FROM mode WHERE mode=1 IN (SELECT mode FROM mode GROUP BY name)");
+    $stmt->setFetchMode(PDO::FETCH_NUM);
+    $stmt->execute();
+    array_push($jsonData['teams'], $stmt->fetch()[0]);
+    
+    $stmt = $db->prepare("SELECT COUNT(*) FROM mode WHERE mode!=1 IN (SELECT mode FROM mode GROUP BY name)");
+    $stmt->setFetchMode(PDO::FETCH_NUM);
+    $stmt->execute();
+    array_push($jsonData['teams'], $stmt->fetch()[0]);
+    
+    $jsonData['game'] = array();
+    
+    $stmt = $db->prepare("SELECT COUNT(*) FROM tlranks.mode WHERE game='W'");
+    $stmt->setFetchMode(PDO::FETCH_NUM);
+    $stmt->execute();
+    array_push($jsonData['game'], $stmt->fetch()[0]);
+    
+    $stmt = $db->prepare("SELECT COUNT(*) FROM tlranks.mode WHERE game='H'");
+    $stmt->setFetchMode(PDO::FETCH_NUM);
+    $stmt->execute();
+    array_push($jsonData['game'], $stmt->fetch()[0]);
+    
+    $stmt = $db->prepare("SELECT COUNT(*) FROM tlranks.users WHERE profile != ''");
+    $stmt->setFetchMode(PDO::FETCH_NUM);
+    $stmt->execute();
+    $jsonData['users'] = $stmt->fetch()[0];
+    $stmt = $db->prepare("SELECT COUNT(DISTINCT name) FROM tlranks.mode");
+    $stmt->setFetchMode(PDO::FETCH_NUM);
+    $stmt->execute();
+    $jsonData['usersWithGames'] = $stmt->fetch()[0];
+    
+    echo json_encode($jsonData);
+  } catch(PDOException $e) {
+    echo $e->getMessage();
+  }
+  $db = null;
+?>
