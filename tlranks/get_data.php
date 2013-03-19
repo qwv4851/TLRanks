69380d518bf1690c12a8ae3f66adb35bef6c9aa7
Sean Reilly <qwv4851@gmail.com>
2013-03-18 23:37:32 -0700

    Workaround for weird php array index bug.

--- tlranks/get_data.php
+++ tlranks/get_data.php
@@ -19,33 +19,39 @@ require 'database.php';
     $stmt = $db->prepare("SELECT COUNT(*) FROM mode WHERE mode=1 IN (SELECT mode FROM mode GROUP BY name)");
     $stmt->setFetchMode(PDO::FETCH_NUM);
     $stmt->execute();
-    array_push($jsonData['teams'], $stmt->fetch()[0]);
+    $res = $stmt->fetch();
+    array_push($jsonData['teams'], $res[0]);
     
     $stmt = $db->prepare("SELECT COUNT(*) FROM mode WHERE mode!=1 IN (SELECT mode FROM mode GROUP BY name)");
     $stmt->setFetchMode(PDO::FETCH_NUM);
     $stmt->execute();
-    array_push($jsonData['teams'], $stmt->fetch()[0]);
+    $res = $stmt->fetch();
+    array_push($jsonData['teams'], $res[0]);
     
     $jsonData['game'] = array();
     
     $stmt = $db->prepare("SELECT COUNT(*) FROM tlranks.mode WHERE game='W'");
     $stmt->setFetchMode(PDO::FETCH_NUM);
     $stmt->execute();
-    array_push($jsonData['game'], $stmt->fetch()[0]);
+    $res = $stmt->fetch();
+    array_push($jsonData['game'], $res[0]);
     
     $stmt = $db->prepare("SELECT COUNT(*) FROM tlranks.mode WHERE game='H'");
     $stmt->setFetchMode(PDO::FETCH_NUM);
     $stmt->execute();
-    array_push($jsonData['game'], $stmt->fetch()[0]);
+    $res = $stmt->fetch();
+    array_push($jsonData['game'], $res[0]);
     
     $stmt = $db->prepare("SELECT COUNT(*) FROM tlranks.users WHERE profile != ''");
     $stmt->setFetchMode(PDO::FETCH_NUM);
     $stmt->execute();
-    $jsonData['users'] = $stmt->fetch()[0];
+    $res = $stmt->fetch();
+    $jsonData['users'] = $res[0];
     $stmt = $db->prepare("SELECT COUNT(DISTINCT name) FROM tlranks.mode");
     $stmt->setFetchMode(PDO::FETCH_NUM);
     $stmt->execute();
-    $jsonData['usersWithGames'] = $stmt->fetch()[0];
+    $res = $stmt->fetch();
+    $jsonData['usersWithGames'] = $res[0];
     
     echo json_encode($jsonData);
   } catch(PDOException $e) {
